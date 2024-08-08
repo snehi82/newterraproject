70a4176bd3c45a09571015aa2a0ec2fe7b17b136
snehi82 <ramavathrajesh82@gmail.com>
2024-08-08 12:04:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 #this file consists of code for instances and sg
 provider "aws" {
-region = "ap-south-1"
+region = "us-east-1"
 }
 
 resource "aws_instance" "one" {
-  ami             = "ami-0a2457eba250ca23d"
+  ami             = "ami-03972092c42e8c0ca"
   instance_type   = "t2.micro"
   key_name        = "terrakeypair"
   vpc_security_group_ids = [aws_security_group.three.id]
-  availability_zone = "ap-south-1a"
+  availability_zone = "us-east-1"
   user_data       = <<EOF
 #!/bin/bash
 sudo -i
@@ -23,18 +23,18 @@ EOF
 }
 
 resource "aws_instance" "two" {
-  ami             = "ami-0a2457eba250ca23d"
+  ami             = "ami-03972092c42e8c0ca"
   instance_type   = "t2.micro"
   key_name        = "terrakeypair"
   vpc_security_group_ids = [aws_security_group.three.id]
-  availability_zone = "ap-south-1b"
+  availability_zone = "us-east-1"
   user_data       = <<EOF
 #!/bin/bash
 sudo -i
 yum install httpd -y
 systemctl start httpd
 chkconfig httpd on
-echo "hai all this is my website created by terraform infrastructurte by raham sir server-2" > /var/www/html/index.html
+echo "hai all this is my website created by terraform infrastructurte by rajesh server-2" > /var/www/html/index.html
 EOF
   tags = {
     Name = "server-2"
